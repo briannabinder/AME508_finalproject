93d3a90824cdb1f87b0ab7b7216e7af09a1d782e
briannabinder <60800838+briannabinder@users.noreply.github.com>
2023-11-29 18:40:19 -0800

    counts num symptoms

--- relabel.py
+++ relabel.py
@@ -16,11 +16,12 @@ def convert_label(file_path):
                 label[1] = 1
             elif first_word == "Wheeze":
                 label[2] = 1
+    # determine the number of each symptom
     return label 
 
 def get_label(file):
     filename = file.replace(".wav", "")
-    label_path = "train/" + filename + "_label.txt"
+    label_path = source + "/" + filename + "_label.txt"
     label = convert_label(label_path)
     return label
 
@@ -29,6 +30,11 @@ def process_data(src_folder, dest_folder):
         shutil.rmtree(dest_folder)
         print(f"Removed existing directory")
     os.makedirs(dest_folder)
+
+    total = 0
+    rhonchi = 0
+    stridor = 0
+    wheeze = 0
 
     counter = 1
     for file in os.listdir(src_folder):
@@ -41,10 +47,26 @@ def process_data(src_folder, dest_folder):
             label = get_label(file)
             np.save(label_path, label)
 
-            counter += 1
+            total += 1
+            if label[0] == 1:
+                rhonchi += 1
+            if label[1] == 1:
+                stridor += 1
+            if label[2] == 1:
+                wheeze += 1
 
-source = "train"
-process_data(source, "train2")
+            counter += 1
+    
+    return total, rhonchi, stridor, wheeze
+
+source = "test"
+total, rhonchi, stridor, wheeze = process_data(source, "test2")
+
+print(f"Total: {total}")
+print(f"Rhonchi: {rhonchi}")
+print(f"Stridor: {stridor}")
+print(f"Wheeze: {wheeze}")
+
 
 # label_file = "train/trunc_2019-07-31-10-20-16-L6_13_label.txt"
 # print(convert_label(label_file))
